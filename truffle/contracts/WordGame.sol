e9ca9161b3429b4f9229be313e71394f40142e0a
harsharora21 <harsharora1337@gmail.com>
2022-12-05 23:27:35 +0530

    barebones game code

--- truffle/contracts/WordGame.sol
+++ truffle/contracts/WordGame.sol
@@ -1,8 +1,37 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0+
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
+//import '@openzeppelin/contracts/access/Ownable.sol';
+import "./strings.sol";
 
-contract WordGame is Ownable {
-  constructor() public {}
+contract WordGame {
+
+	using strings for *;
+
+	string word;
+
+	constructor() 
+	{
+		word = "abdakdabra";
+	}
+	
+	function getWord() public view returns(string memory)
+	{
+		return word;
+	}
+	
+	function sendWord(string memory newWord) public returns (bool sufficient)
+	{
+		strings.slice memory startNew = newWord.toSlice();
+		startNew._len=1;
+		
+		if(word.toSlice().endsWith(startNew))
+		{
+			word = newWord;
+			return true;
+		}
+
+		return false;
+	}
+
 }
