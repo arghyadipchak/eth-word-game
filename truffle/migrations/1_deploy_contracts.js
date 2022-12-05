bfcf4b6663ae6df7a93fc0ebb0d336130e9aa29c
harsharora21 <harsharora1337@gmail.com>
2022-12-05 23:28:02 +0530

    updated deploy code

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -1,6 +1,9 @@
+const StringSol = artifacts.require("strings");
 const WordGame = artifacts.require("WordGame");
 
 module.exports = function(deployer)
 {
+	deployer.deploy(StringSol);
+	deployer.link(StringSol,WordGame);
 	deployer.deploy(WordGame);
 }
