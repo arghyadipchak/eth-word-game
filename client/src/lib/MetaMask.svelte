34bd11e68ccbf57031366d28f9602a4c6cc3e56b
Paarth314 <paarthiyer1234@gmail.com>
2022-12-06 12:15:05 +0530

    metamask wallet connect prompt

--- client/src/lib/MetaMask.svelte
+++ client/src/lib/MetaMask.svelte
@@ -1,4 +1,33 @@
-{#if window.ethereum === undefined}
+<script>
+  import { onMount } from 'svelte'
+
+  let MetaMaskConnected = false
+  let WalletConnected = window.ethereum.selectedAddress !== null
+
+  // const onClickInstall = () => {
+  //   onboardButton.innerText = 'Onboarding in progress';
+  //   onboardButton.disabled = true;
+  //   //On this object we have startOnboarding which will start the onboarding process for our end user
+  //   onboarding.startOnboarding();
+  // };
+
+  onMount(async () => {
+    if (!(window.ethereum === undefined) && window.ethereum.isMetaMask) {
+      MetaMaskConnected = true
+      try {
+        await window.ethereum.request({ method: 'eth_requestAccounts' })
+      } catch (error) {
+        console.log('Refresh page')
+      }
+    }
+  })
+
+  window.ethereum.on('accountsChanged', function (accounts) {
+    WalletConnected = true
+  })
+</script>
+
+{#if !MetaMaskConnected}
   <input type="checkbox" id="my-modal" class="modal-toggle" checked={true} />
   <div class="modal">
     <div class="modal-box flex flex-col items-center text-center">
@@ -13,6 +42,24 @@
           rel="noreferrer"
           class="btn">Install MetaMask</a
         >
+      </div>
+    </div>
+  </div>
+{:else if !WalletConnected}
+  <input type="checkbox" id="my-modal" class="modal-toggle" checked={true} />
+  <div class="modal">
+    <div class="modal-box flex flex-col items-center text-center">
+      <h3 class="font-bold text-lg text-error">
+        Please connect your MetaMask account
+      </h3>
+      <div class="modal-action">
+        <div class="grid gap-2">
+          <div class="flex items-center justify-center space-x-2 animate-pulse">
+            <div class="w-8 h-8 bg-red-500 rounded-full" />
+            <div class="w-8 h-8 bg-red-500 rounded-full" />
+            <div class="w-8 h-8 bg-red-500 rounded-full" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
