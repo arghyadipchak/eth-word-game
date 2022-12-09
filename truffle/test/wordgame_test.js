8485fcf3b0ed97bc37fe9c58227097d5f1426bda
harsharora21 <harsharora1337@gmail.com>
2022-12-09 13:36:54 +0530

    added tests

--- truffle/test/wordgame_test.js
+++ truffle/test/wordgame_test.js
@@ -70,7 +70,156 @@ contract('WordGame: let us play', (acc) => {
     await game.methods.sendWord('kanye').send({from:acc[1]});
     await game.methods.sendWord('elon').send({from:acc[2]});
     
-    assert.equal(await game.methods.getIsGameEnd().call(),true,'game has NOT ended yet');
+    assert.equal(await game.methods.hasGameEnded().call(),true,'game has NOT ended yet');
 
-  })
+  });
+
+  it('leave game tests: everyone leaves', async() => {
+    let fac = await WordGameFactory.deployed();
+    await fac.newGame(1);
+    addr = await fac.getLastGame();
+    game = new web3.eth.Contract(WordGame.abi);
+    game.options.address=addr;
+
+    //Will start to play a game now
+
+    //3 people play a game
+    await game.methods.joinGame().send({from:acc[0]});
+    await game.methods.joinGame().send({from:acc[1]});
+    await game.methods.joinGame().send({from:acc[2]});
+
+    await game.methods.startGame().send({from:acc[0]});
+
+    await game.methods.leaveGame().send({from:acc[1]});
+
+    // acc[1] should no longer be a player
+    assert.equal(await game.methods.checkIfPlayer(acc[1]).call(),false,'acc[1] is a player');
+    
+    // acc[0] sends word
+
+    await game.methods.sendWord('abs').send({from:acc[0]});
+    // turn of acc[2]
+    assert.equal(await game.methods.getTurn().call(),2,'turn of acc[2] did NOT come');
+    
+    // acc[2] leaves
+    await game.methods.leaveGame().send({from:acc[2]});
+    // game should end
+    assert.equal(await game.methods.hasGameEnded().call(),true,'game has NOT ended');
+    
+  });
+
+  it('leave game tests: some people leave', async() => {
+    let fac = await WordGameFactory.deployed();
+    await fac.newGame(2);
+    addr = await fac.getLastGame();
+    game = new web3.eth.Contract(WordGame.abi);
+    game.options.address=addr;
+    game.options.gas = 5000000;
+
+    //Will start to play a game now
+
+    //3 people play a game
+    await game.methods.joinGame().send({from:acc[0]});
+    await game.methods.joinGame().send({from:acc[1]});
+    await game.methods.joinGame().send({from:acc[2]});
+
+    await game.methods.startGame().send({from:acc[0]});
+
+    
+
+    // acc[0] sends word
+    await game.methods.sendWord('abs').send({from:acc[0]});
+
+    //acc[1] leaves game
+    await game.methods.leaveGame().send({from:acc[1]});
+    // acc[1] should no longer be a player
+    assert.equal(await game.methods.checkIfPlayer(acc[1]).call(),false,'acc[1] is a player');
+    
+    // turn of acc[2]
+    await game.methods.sendWord('story').send({from:acc[2], gas: 5000000});
+    assert.equal(await game.methods.getWord().call(),'story','acc[2] was NOT able to send word');
+
+    // turn of acc[0]
+    await game.methods.sendWord('yak').send({from:acc[0]});
+    assert.equal(await game.methods.getWord().call(),'yak','acc[0] was NOT able to send word');
+    
+    // acc[2] leaves
+    await game.methods.leaveGame().send({from:acc[2]});
+    // game should end
+    assert.equal(await game.methods.hasGameEnded().call(),true,'game has NOT ended');
+    
+  });
+
+  it('leave game tests: some people leave 2', async() => {
+    let fac = await WordGameFactory.deployed();
+    await fac.newGame(2);
+    addr = await fac.getLastGame();
+    game = new web3.eth.Contract(WordGame.abi);
+    game.options.address=addr;
+    game.options.gas = 5000000;
+
+    //Will start to play a game now
+
+    //3 people play a game
+    await game.methods.joinGame().send({from:acc[0]});
+    await game.methods.joinGame().send({from:acc[1]});
+    await game.methods.joinGame().send({from:acc[2]});
+
+    await game.methods.startGame().send({from:acc[0]});
+
+    
+
+    // acc[0] sends word
+    await game.methods.sendWord('abs').send({from:acc[0]});
+
+    // turn of acc[1]
+    await game.methods.sendWord('story').send({from:acc[1]});
+    assert.equal(await game.methods.getWord().call(),'story','acc[1] was NOT able to send word');
+    
+    //acc[2] leaves game
+    await game.methods.leaveGame().send({from:acc[2]});
+    // acc[2] should no longer be a player
+    assert.equal(await game.methods.checkIfPlayer(acc[2]).call(),false,'acc[2] is a player');
+
+    // turn of acc[0]
+    await game.methods.sendWord('yak').send({from:acc[0]});
+    assert.equal(await game.methods.getWord().call(),'yak','acc[0] was NOT able to send word');
+    
+  });
+
+  it('leave game tests: some people leave 3', async() => {
+    let fac = await WordGameFactory.deployed();
+    await fac.newGame(1);
+    addr = await fac.getLastGame();
+    game = new web3.eth.Contract(WordGame.abi);
+    game.options.address=addr;
+    game.options.gas = 5000000;
+
+    //Will start to play a game now
+
+    //3 people play a game
+    await game.methods.joinGame().send({from:acc[0]});
+    await game.methods.joinGame().send({from:acc[1]});
+    await game.methods.joinGame().send({from:acc[2]});
+
+    await game.methods.startGame().send({from:acc[0]});
+
+    
+
+    // acc[0] sends word
+    await game.methods.sendWord('abs').send({from:acc[0]});
+
+    // turn of acc[1]
+    await game.methods.sendWord('story').send({from:acc[1]});
+    assert.equal(await game.methods.getWord().call(),'story','acc[1] was NOT able to send word');
+    
+    //acc[2] leaves game
+    await game.methods.leaveGame().send({from:acc[2]});
+    // acc[2] should no longer be a player
+    assert.equal(await game.methods.checkIfPlayer(acc[2]).call(),false,'acc[2] is a player');
+
+    // game should end
+    assert.equal(await game.methods.hasGameEnded().call(),true,'game has NOT ended yet');
+    
+  });
 })
